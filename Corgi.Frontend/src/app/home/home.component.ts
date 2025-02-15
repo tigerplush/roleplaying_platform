7985d746af9be85283db508ba16d5571de260158
tigerplush <christopher.putz@corgibase.de>
2025-02-15 17:18:13 +0100

    added home component

---
+++ Corgi.Frontend/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  ngOnInit() {
+    console.log(environment.keycloakUrl);
+  }
+}
